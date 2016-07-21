f6368a1daa676dcd3a529575c8fba2d90c29b6b8
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-20 18:17:34 -0700

    Added in sample project to test proxy

---
+++ PoGo Proxy.Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace PoGo_Proxy.Sample
+{
+    class Program
+    {
+        private static readonly ProxyController Controller = new ProxyController();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hit any key to exit..");
+            Console.WriteLine();
+
+            Controller.Start();
+
+            Console.Read();
+
+            Controller.Stop();
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), DateTime.Now.ToString("yyyy-MM-dd-hh-mm") + "-log.json");
+            File.WriteAllText(path, Controller.ApiLogger.ToString());
+        }
+    }
+}
