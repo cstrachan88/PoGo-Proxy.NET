81008a47b504670821bf57134cefdff0b8a7623c
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-22 14:03:06 -0700

    Made the output messages human readable

--- PoGo Proxy.Sample/Program.cs
+++ PoGo Proxy.Sample/Program.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using Google.Protobuf;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using POGOProtos.Map;
+using POGOProtos.Map.Pokemon;
 using POGOProtos.Networking.Requests;
 using POGOProtos.Networking.Responses;
 
 namespace PoGo_Proxy.Sample
 {
-    class Program
+    internal class Program
     {
         private static List<RequestHandledEventArgs> _apiLog;
+        private static List<Tuple<RequestType, string>> _outputMessages;
+        private static Dictionary<ulong, NearbyPokemon> _nearbyPokemon;
 
-        static void Main()
+        private static void Main()
         {
             _apiLog = new List<RequestHandledEventArgs>();
+            _outputMessages = new List<Tuple<RequestType, string>>();
+            _nearbyPokemon = new Dictionary<ulong, NearbyPokemon>();
 
             Console.WriteLine("Hit any key to stop proxy..");
             Console.WriteLine();
 
             var controller = new ProxyController("192.168.0.19", 61212) {Out = Console.Out};
-            
+
             controller.RequestHandled += Controller_RequestHandled;
 
             controller.Start();
@@ -41,33 +49,158 @@ namespace PoGo_Proxy.Sample
             // Update log
             _apiLog.Add(e);
 
-            // Using stringbuilder to group response blocks since messages can get overlayed due to threading
-            var sb = new StringBuilder();
-
-            // Parse responses
-            sb.AppendLine("[<-] Response blocks:");
+            // Print response to console only if new
             foreach (var responsePair in e.ResponseBlock.ParsedMessages)
             {
-                sb.AppendLine(" [+] " + responsePair.Key);
-
                 switch (responsePair.Key)
                 {
-                    case RequestType.Encounter:
-                        sb.AppendLine("     Name : " + ((EncounterResponse)responsePair.Value).WildPokemon.PokemonData.PokemonId);
-                        sb.AppendLine("     Attack : " + ((EncounterResponse)responsePair.Value).WildPokemon.PokemonData.IndividualAttack);
-                        sb.AppendLine("     Defense : " + ((EncounterResponse)responsePair.Value).WildPokemon.PokemonData.IndividualDefense);
-                        sb.AppendLine("     Stamina : " + ((EncounterResponse)responsePair.Value).WildPokemon.PokemonData.IndividualStamina);
-                        break;
+                    case RequestType.GetMapObjects:
+                        ParseMapObject((GetMapObjectsResponse) responsePair.Value);
+                        continue;
+                    case RequestType.DownloadSettings:
+                    case RequestType.GetAssetDigest:
+                    case RequestType.DownloadRemoteConfigVersion:
+                        continue;
+                }
 
-                    case RequestType.DiskEncounter:
-                        sb.AppendLine("     Name : " + ((DiskEncounterResponse)responsePair.Value).PokemonData.PokemonId);
-                        sb.AppendLine("     Attack : " + ((DiskEncounterResponse)responsePair.Value).PokemonData.IndividualAttack);
-                        sb.AppendLine("     Defense : " + ((DiskEncounterResponse)responsePair.Value).PokemonData.IndividualDefense);
-                        sb.AppendLine("     Stamina : " + ((DiskEncounterResponse)responsePair.Value).PokemonData.IndividualStamina);
-                        break;
+                var output = _outputMessages.SingleOrDefault(tuple => tuple.Item1 == responsePair.Key);
+
+                var outputMessage = ParseResponseMessage(responsePair);
+
+                if (output == null)
+                {
+                    _outputMessages.Add(new Tuple<RequestType, string>(responsePair.Key, outputMessage));
+
+                    Console.WriteLine($"[+] New {responsePair.Key} response");
+                    Console.WriteLine(outputMessage);
+                }
+                else
+                {
+                    if (output.Item2 == outputMessage) continue;
+
+                    Console.WriteLine($"[+] Updated {responsePair.Key} response");
+                    Console.WriteLine(outputMessage);
                 }
             }
-            Console.WriteLine(sb.ToString());
+        }
+
+        private static void ParseMapObject(GetMapObjectsResponse response)
+        {
+            if (response.Status == MapObjectsStatus.Success)
+            {
+                foreach (var cell in response.MapCells)
+                {
+                    foreach (var poke in cell.NearbyPokemons)
+                    {
+                        _nearbyPokemon[poke.EncounterId] = poke;
+                    }
+                }
+            }
+
+            if (_nearbyPokemon.Count > 0) Console.WriteLine("NEARBY POKEMON");
+            foreach (var poke in _nearbyPokemon.Values)
+            {
+                Console.WriteLine($"  Name: {poke.PokemonId}, Distance: {poke.DistanceInMeters}m (This value will always be 200m until Niantic fixes servers)");
+            }
+        }
+
+        private static string ParseResponseMessage(KeyValuePair<RequestType, IMessage> responsePair)
+        {
+            var sb = new StringBuilder();
+
+            switch (responsePair.Key)
+            {
+                default:
+                    sb.AppendLine("    Ignored");
+                    break;
+
+                case RequestType.GetPlayer:
+                {
+                    var response = (GetPlayerResponse) responsePair.Value;
+                    if (response.Success)
+                    {
+                        sb.AppendLine($"    Name: {response.PlayerData.Username}");
+                        foreach (var currencyPair in response.PlayerData.Currencies)
+                        {
+                            sb.AppendLine($"    {currencyPair.Name}: {currencyPair.Amount}");
+                        }
+                    }
+                    break;
+                }
+                case RequestType.GetHatchedEggs:
+                {
+                    var response = (GetHatchedEggsResponse) responsePair.Value;
+                    if (response.Success)
+                    {
+                        if (response.PokemonId.Count == 0) sb.AppendLine("    No hatched eggs");
+                        for (int i = 0; i < response.PokemonId.Count; i++)
+                        {
+                            sb.AppendLine($"    Egg {i}: ");
+                            sb.AppendLine("      PokemonId: " + response.PokemonId[i]);
+                            sb.AppendLine("      ExperienceAwarded: " + response.ExperienceAwarded[i]);
+                            sb.AppendLine("      CandyAwarded: " + response.CandyAwarded[i]);
+                            sb.AppendLine("      StardustAwarded: " + response.StardustAwarded[i]);
+                        }
+                    }
+                    break;
+                }
+                case RequestType.GetInventory:
+                {
+                    var response = (GetInventoryResponse) responsePair.Value;
+                    if (response.Success)
+                    {
+                        sb.AppendLine("    lots of data to parse");
+                    }
+                    break;
+                }
+                case RequestType.CheckAwardedBadges: // TODO should this be GetAwardedBadges in POGOProtocs?
+                {
+                    var response = (CheckAwardedBadgesResponse) responsePair.Value;
+                    if (response.Success)
+                        {
+                            if (response.AwardedBadges.Count == 0) sb.AppendLine("    No awarded badges");
+                            for (int i = 0; i < response.AwardedBadges.Count; i++)
+                        {
+                            sb.AppendLine($"    Badge {i}: ");
+                            sb.AppendLine("      AwardedBadges: " + response.AwardedBadges[i]);
+                            sb.AppendLine("      AwardedBadgeLevels: " + response.AwardedBadgeLevels[i]);
+                        }
+                    }
+                    break;
+                }
+                case RequestType.GetGymDetails:
+                {
+                    var response = (GetGymDetailsResponse) responsePair.Value;
+
+                    if (response.Result == GetGymDetailsResponse.Types.Result.Success)
+                    {
+                        sb.AppendLine($"    Name: {response.Name}");
+                        sb.AppendLine($"    Id: {response.GymState.FortData.Id}");
+                    }
+                    break;
+                }
+                case RequestType.Encounter:
+                {
+                    var response = (EncounterResponse) responsePair.Value;
+
+                    sb.AppendLine("    PokemonId: " + response.WildPokemon.PokemonData.PokemonId);
+                    sb.AppendLine("    IndividualAttack: " + response.WildPokemon.PokemonData.IndividualAttack);
+                    sb.AppendLine("    IndividualDefense: " + response.WildPokemon.PokemonData.IndividualDefense);
+                    sb.AppendLine("    IndividualStamina: " + response.WildPokemon.PokemonData.IndividualStamina);
+                    break;
+                }
+                case RequestType.DiskEncounter:
+                {
+                    var response = (DiskEncounterResponse) responsePair.Value;
+
+                    sb.AppendLine("    PokemonId: " + response.PokemonData.PokemonId);
+                    sb.AppendLine("    IndividualAttack: " + response.PokemonData.IndividualAttack);
+                    sb.AppendLine("    IndividualDefense: " + response.PokemonData.IndividualDefense);
+                    sb.AppendLine("    IndividualStamina: " + response.PokemonData.IndividualStamina);
+                    break;
+                }
+            }
+            return sb.ToString();
         }
     }
 }
