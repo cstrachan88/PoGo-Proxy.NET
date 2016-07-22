917baa813213885c4a9012329ceec67f1ebf4ce7
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-22 12:38:32 -0700

    Refactored ProxyController to better handle unexpected events

--- PoGo Proxy/ProxyController.cs
+++ PoGo Proxy/ProxyController.cs
@@ -19,11 +19,11 @@ namespace PoGo_Proxy
         private readonly ProxyServer _proxyServer;
         private readonly Dictionary<ulong, RequestHandledEventArgs> _apiBlocks;
 
+        public event EventHandler<RequestHandledEventArgs> RequestHandled;
+
         public string Ip { get; }
         public int Port { get; }
         public TextWriter Out { get; set; }
-
-        public event EventHandler<RequestHandledEventArgs> RequestHandled;
 
         public ProxyController(string ipAddress, int port)
         {
@@ -90,38 +90,47 @@ namespace PoGo_Proxy
             // Parse all the requests
             foreach (Request request in requestEnvelope.Requests)
             {
-                // Had to add assembly name to end of typeName string since protocs.cs files are in a different assembly
+                // Had to add assembly name to end of typeName string since protocs cs files are in a different assembly
                 var type = Type.GetType("POGOProtos.Networking.Requests.Messages." + request.RequestType + "Message,PoGo Proxy.Protocs");
 
                 if (type == null)
                 {
-                    if (Out != StreamWriter.Null) Out.WriteLine("[***] GetType returns null for requestType: " + request.RequestType);
-                    if (Out != StreamWriter.Null) Out.WriteLine("[***] Check if POGOProtos.Networking.Requests.Messages." + request.RequestType + "Message exists.");
+                    if (Out != StreamWriter.Null) Out.WriteLine("[*] GetType returns null for requestType: " + request.RequestType);
+                    if (Out != StreamWriter.Null) Out.WriteLine("[*] Check if POGOProtos.Networking.Requests.Messages." + request.RequestType + "Message exists.");
+                    if (Out != StreamWriter.Null) Out.WriteLine("[*]\n");
+
+                    requestBlock.ParsedMessages.Add(request.RequestType, default(IMessage));
                 }
+                else
+                {
+                    var instance = (IMessage) Activator.CreateInstance(type);
+                    instance.MergeFrom(request.RequestMessage);
 
-                var instance = (IMessage)Activator.CreateInstance(type);
-                instance.MergeFrom(request.RequestMessage);
-
-                requestBlock.ParsedMessages.Add(request.RequestType, instance);
+                    requestBlock.ParsedMessages.Add(request.RequestType, instance);
+                }
             }
 
-            // TODO figure out why the requests are doubling up and what to do about it
+            // TODO check if there is a double response or if a response already exists
             if (_apiBlocks.ContainsKey(requestEnvelope.RequestId))
             {
-                // If the requests are the same, no need to readd
-                if (_apiBlocks[requestEnvelope.RequestId].RequestBlock.ParsedMessages.Equals(requestBlock.ParsedMessages)) return;
-
                 if (Out != StreamWriter.Null)
                 {
-                    Out.WriteLine("[*]");
                     Out.WriteLine($"[*] Request Id({requestEnvelope.RequestId}) already exists.");
-
                     Out.WriteLine($"[*] Old request:\n{_apiBlocks[requestEnvelope.RequestId].RequestBlock}");
                     Out.WriteLine($"[*] New request:\n{requestBlock}");
-
-                    Out.WriteLine("[*]");
                 }
-                //throw new ArgumentException($"Request Id ({requestEnvelope.RequestId}) already exists.");
+
+                if (_apiBlocks[requestEnvelope.RequestId].ResponseBlock == null)
+                {
+                    if (Out != StreamWriter.Null) Out.WriteLine($"[*] Response for the old request doesn't - replacing old request");
+                    _apiBlocks[requestEnvelope.RequestId].RequestBlock = requestBlock;
+                }
+                else
+                {
+                    if (Out != StreamWriter.Null) Out.WriteLine($"[*] Response for the old request exists - do nothing");
+                }
+                Out.WriteLine("[*]\n");
+                return;
             }
 
             // Initialize a new request/response paired block and track it to update response
@@ -131,8 +140,6 @@ namespace PoGo_Proxy
                 RequestBlock = requestBlock
             };
             _apiBlocks.Add(args.RequestId, args);
-
-            //if (Out != StreamWriter.Null) Out.WriteLine(requestBlock);
         }
 
         private async Task OnResponse(object sender, SessionEventArgs e)
@@ -157,6 +164,9 @@ namespace PoGo_Proxy
                 // Grab the paired request
                 var args = _apiBlocks[responseEnvelope.RequestId];
 
+                // Grab request types
+                var requestTypes = args.RequestBlock.ParsedMessages.Keys.ToList();
+
                 // The case of missmatched requests and responses seems to be a handshake. The inital request sends 5 messages and gets back 2 that are empty bytestrings
                 if (args.RequestBlock.ParsedMessages.Count != responseEnvelope.Returns.Count)
                 {
@@ -169,7 +179,6 @@ namespace PoGo_Proxy
                     }
 
                     // If there is a case of missmatched requests and responses, and it doesn't look like a handshake, log it
-
                     if (Out != StreamWriter.Null)
                     {
                         Out.WriteLine($"[*] Request messages count ({args.RequestBlock.ParsedMessages.Count}) is different than the response messages count ({responseEnvelope.Returns.Count}).");
@@ -182,26 +191,24 @@ namespace PoGo_Proxy
 
                         Out.WriteLine($"[*]\n");
                     }
-                    //throw new RankException("Request messages count is different than the response messages count.");
 
-                    // TODO Going to try and parse the responses to each type, and see which one fits
-                    var requestTypesTest = args.RequestBlock.ParsedMessages.Keys.ToList();
+                    // This section converts all of the responses into all of the request types that exist to see which one fits
                     int responseIndex = 0;
 
                     foreach (var responseBytes in responseEnvelope.Returns)
                     {
                         int start = responseIndex;
-                        int end = responseIndex + requestTypesTest.Count - responseEnvelope.Returns.Count;
+                        int end = responseIndex + requestTypes.Count - responseEnvelope.Returns.Count;
 
                         // Parse the responses
                         for (int i = start; i < end; i++)
                         {
-                            var type = Type.GetType("POGOProtos.Networking.Responses." + requestTypesTest[i] + "Response,PoGo Proxy.Protocs");
+                            var type = Type.GetType("POGOProtos.Networking.Responses." + requestTypes[i] + "Response,PoGo Proxy.Protocs");
 
                             var instance = (IMessage) Activator.CreateInstance(type);
                             instance.MergeFrom(responseBytes);
 
-                            Out.WriteLine($"[*] Parsing as response {responseIndex} as {requestTypesTest[i]}");
+                            Out.WriteLine($"[*] Parsing as response {responseIndex} as {requestTypes[i]}");
                             Out.WriteLine(JsonConvert.SerializeObject(instance));
                         }
 
@@ -210,9 +217,6 @@ namespace PoGo_Proxy
                     }
                     Out.WriteLine($"[*]\n");
                 }
-
-                // Grab request types
-                var requestTypes = args.RequestBlock.ParsedMessages.Keys.ToList();
 
                 // Parse the responses
                 for (int i = 0; i < responseEnvelope.Returns.Count; i++)
@@ -224,35 +228,35 @@ namespace PoGo_Proxy
                     {
                         if (Out != StreamWriter.Null) Out.WriteLine("[***] GetType returns null for requestType: " + requestTypes[i]);
                         if (Out != StreamWriter.Null) Out.WriteLine("[***] Check if POGOProtos.Networking.Requests.Messages." + requestTypes[i] + "Message exists.");
+                        if (Out != StreamWriter.Null) Out.WriteLine("[*]\n");
+
+                        responseBlock.ParsedMessages.Add(requestTypes[i], default(IMessage));
                     }
+                    else
+                    {
+                        var instance = (IMessage)Activator.CreateInstance(type);
+                        instance.MergeFrom(responseEnvelope.Returns[i]);
 
-                    var instance = (IMessage)Activator.CreateInstance(type);
-                    instance.MergeFrom(responseEnvelope.Returns[i]);
-
-                    responseBlock.ParsedMessages.Add(requestTypes[i], instance);
+                        responseBlock.ParsedMessages.Add(requestTypes[i], instance);
+                    }
                 }
 
-                // TODO what scenarios would cause this
+                // Have not seen this issue yet - here just in case
                 if (!_apiBlocks.ContainsKey(responseEnvelope.RequestId))
                 {
                     if (Out != StreamWriter.Null)
                     {
-                        Out.WriteLine("[*]");
                         Out.WriteLine($"[*] Request doesn't exist with specified RequestId ({responseEnvelope.RequestId}).");
-
-                        Out.WriteLine($"[*] Response:\n{responseBlock}");
-
-                        Out.WriteLine("[*]");
+                        Out.WriteLine($"Response:\n{responseBlock}");
+                        Out.WriteLine("[*]\n");
                     }
-                    //throw new KeyNotFoundException($"Request doesn't exist with specified RequestId ({responseEnvelope.RequestId}).");
                 }
 
                 // Remove block from dictionary and invoke event handler
                 args.ResponseBlock = responseBlock;
                 _apiBlocks.Remove(args.RequestId);
-                RequestHandled?.Invoke(this, args);
 
-                //if (Out != StreamWriter.Null) Out.WriteLine(responseBlock);
+                RequestHandled?.Invoke(this, args);
             }
         }
 
