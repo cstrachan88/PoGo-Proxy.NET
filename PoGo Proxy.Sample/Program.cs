e155d42be6f39c9df993cb1fc4ee67549f3129f0
Courtney Strachan <courtney.strachan@gmail.com>
2016-07-21 18:09:37 -0700

    Added in api log

--- PoGo Proxy.Sample/Program.cs
+++ PoGo Proxy.Sample/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using POGOProtos.Networking.Requests;
 using POGOProtos.Networking.Responses;
 
@@ -7,9 +10,13 @@ namespace PoGo_Proxy.Sample
 {
     class Program
     {
+        private static List<ResponseEventArgs> _apiLog;
+
         static void Main()
         {
-            Console.WriteLine("Hit any key to exit..");
+            _apiLog = new List<ResponseEventArgs>();
+
+            Console.WriteLine("Hit any key to stop proxy..");
             Console.WriteLine();
 
             var controller = new ProxyController("192.168.0.19", 61212)
@@ -20,23 +27,22 @@ namespace PoGo_Proxy.Sample
 
             controller.Start();
 
-            // Press a key to stop proxy, then press a key to exit
             Console.ReadKey();
 
             controller.Stop();
 
+            Console.WriteLine("Hit any key to save log of requests and responses and exit..");
             Console.ReadKey();
+
+            // Log requests and responses
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), DateTime.Now.ToString("yy-MM-dd-hh-mm") + "-log.json"),
+                JsonConvert.SerializeObject(_apiLog, Formatting.Indented, new StringEnumConverter()));
         }
 
         private static void Controller_ResponseReceived(object sender, ResponseEventArgs e)
         {
-            //Console.WriteLine("[<-] Received responses for request id " + e.RequestId);
-
-            //foreach (var response in e.Responses.ParsedMessages)
-            //{
-            //    Console.WriteLine($" [-] Received {response.Key}");
-            //}
-            //Console.WriteLine();
+            // Update log
+            _apiLog.Add(e);
 
             foreach (var responsePair in e.Responses.ParsedMessages)
             {
@@ -48,54 +54,6 @@ namespace PoGo_Proxy.Sample
                     {
                         Console.WriteLine(JsonConvert.SerializeObject(inventory.InventoryDelta));
                     }
-
-                    //Console.WriteLine("Inventory:");
-                    //Console.WriteLine("  " + (inventory.OriginalTimestampMs == 0 ? "First update" : DateHelpers.FromUnixTime(inventory.OriginalTimestampMs).ToString("g")));
-                    //Console.WriteLine("  " + DateHelpers.FromUnixTime(inventory.NewTimestampMs).ToString("g"));
-
-                    //foreach (var inventoryItem in inventory.InventoryItems)
-                    //{
-                    //    if (inventoryItem.InventoryItemData.PokemonData != null)
-                    //    {
-                    //        Console.WriteLine("    PokemonData: " + inventoryItem.InventoryItemData.PokemonData.PokemonId);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.Item != null)
-                    //    {
-                    //        Console.WriteLine("    Item: " + inventoryItem.InventoryItemData.Item.ItemId);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.PokedexEntry != null)
-                    //    {
-                    //        Console.WriteLine("    PokedexEntry: " + inventoryItem.InventoryItemData.PokedexEntry.PokemonId);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.PlayerStats != null)
-                    //    {
-                    //        Console.WriteLine("    PlayerStats: " + inventoryItem.InventoryItemData.PlayerStats.Experience);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.PlayerCurrency != null)
-                    //    {
-                    //        Console.WriteLine("    PlayerCurrency: " + inventoryItem.InventoryItemData.PlayerCurrency.Gems);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.PlayerCamera != null)
-                    //    {
-                    //        Console.WriteLine("    PlayerCamera: " + inventoryItem.InventoryItemData.PlayerCamera.IsDefaultCamera);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.InventoryUpgrades != null)
-                    //    {
-                    //        Console.WriteLine("    InventoryUpgrades: " + inventoryItem.InventoryItemData.InventoryUpgrades.InventoryUpgrades_[0].UpgradeType);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.AppliedItems != null)
-                    //    {
-                    //        Console.WriteLine("    AppliedItems: " + inventoryItem.InventoryItemData.AppliedItems.Item[0].ItemId);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.EggIncubators != null)
-                    //    {
-                    //        Console.WriteLine("    EggIncubators: " + inventoryItem.InventoryItemData.EggIncubators.EggIncubator.IncubatorType);
-                    //    }
-                    //    if (inventoryItem.InventoryItemData.PokemonFamily != null)
-                    //    {
-                    //        Console.WriteLine("    PokemonFamily: " + inventoryItem.InventoryItemData.PokemonFamily.Candy);
-                    //    }
-                    
                 }
             }
         }
